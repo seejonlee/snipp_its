303f9b9f78a12eb1fec27f74b80276df806e3bb1
Jon Lee <jdukelee@gmail.com>
2021-10-28 17:27:50 -0700

    Add: example of setting array of refs in hook form

---
+++ react/hooks/useArrayOfRefs.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+/**
+ * Usage:
+ * const [arrayOfRefs, refCallback] = useArrayOfRefs(someLength);
+ * 
+ * Within a .map (since this would most likely correspond with some array of data your component maps over):
+ * ref={(el) => { refCallback(el, index); }}
+ */
+const useArrayOfRefs = (size) => {
+	const arrayOfRefs = useRef(new Array(size));
+
+	const refCallback = (el, index) => {
+		if (arrayOfRefs.current) {
+		arrayOfRefs.current[index] = el;
+		}
+	};
+
+	return [arrayOfRefs, refCallback];
+};
