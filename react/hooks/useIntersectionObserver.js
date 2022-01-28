6867bfe4a9395d94e893e48cd9d8e22cd3bf24af
Jon Lee <jdukelee@gmail.com>
2022-01-28 09:58:38 -0800

    Add: example comments

--- react/hooks/useIntersectionObserver.js
+++ react/hooks/useIntersectionObserver.js
@@ -1,5 +1,32 @@
 import { useEffect, useRef } from 'react';
 
+/**
+ * Example Usage:
+
+	const topEntryRef = useIntersectionObserver(entry => {
+		console.log(entry.isIntersecting);
+	}, {
+		rootMargin: '10% 0% -60% 0%'
+	});
+
+	Example component to use as an observed element for cases where an existing element is undesirable.
+	import React, {forwardRef} from 'react';
+
+	const IntersectionObserverPixel = (className, ref) => {
+		return (
+			<span
+				style={{
+					width: '1px',
+					height: '1px',
+					visibility: 'hidden',
+				}}
+				ref={ref}
+			/>
+		);
+	};
+
+	export default forwardRef(IntersectionObserverPixel);
+ */
 function useIntersectionObserver(intersectHandler, config) {
 	const observedRef = useRef();
 	useEffect(() => {
